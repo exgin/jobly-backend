81efd7f6fdc69aa2659bf50080d5d3c2bbcab403
unknown <exgin3538@yahoo.com>
2020-06-15 12:32:24 -0400

    add auth functions

---
+++ middleware/middleAuth.js
@@ -0,0 +1,51 @@
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config');
+const ExpressError = require('../helpers/ExpressError');
+
+function authRequired(req, res, next) {
+  try {
+    let verifiedPayload = jwt.verify(req.body._token, SECRET_KEY);
+
+    req.user = verifiedPayload; // create a current user
+
+    return next();
+  } catch (error) {
+    return next();
+  }
+}
+
+function ensureCorrectUser(req, res, next) {
+  try {
+    // if there is not current user, not allows
+    if (!req.user) {
+      throw new ExpressError('Unauthorized', 400);
+    }
+
+    // if username in url is same as user iwthin the global token
+    if (req.params.username !== req.user.username) {
+      throw new ExpressError('Unauthorized', 400);
+    }
+
+    return next();
+  } catch (error) {
+    return next();
+  }
+}
+
+function checkAdmin(req, res, next) {
+  try {
+    if (!req.user) {
+      throw new ExpressError('Unauthorized', 400);
+    }
+
+    if (!req.user.is_admin) {
+      throw new ExpressError('Unauthorized, must be an admin', 400);
+    }
+
+    return next();
+  } catch (error) {
+    return next();
+  }
+}
+
+module.exports = { authRequired, ensureCorrectUser, checkAdmin };
