5ae30d14b01b551425a6a34cd57fce0905bb5109
unknown <exgin3538@yahoo.com>
2020-06-14 23:14:26 -0400

    add patch route for users

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = new Router();
 const User = require('../models/users');
 const { validate } = require('jsonschema');
 const createToken = require('../helpers/createToken');
+const ExpressError = require('../helpers/ExpressError');
 
 // show all users
 router.get('/', async function (req, res, next) {
@@ -27,9 +28,27 @@ router.get('/:username', async function (req, res, next) {
 // create a user
 router.post('/', async function (req, res, next) {
   try {
+    // add schema validation
+
     const user = await User.create(req.body);
     const token = createToken(user);
     return res.status(201).json({ token });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// update a user's info, even their password
+router.patch('/:username', async function (req, res, next) {
+  try {
+    if ('username' in req.body) {
+      throw new ExpressError(`You can't change your username!`, 400);
+    }
+
+    // add schema validation
+
+    const user = await User.update(req.params.username, req.body);
+    return res.json({ user });
   } catch (error) {
     return next(error);
   }
